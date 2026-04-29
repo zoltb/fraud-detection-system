cd6c9c2bb02c75c56820d5df681afe928e9d4fa7
Zoltan Bagyi <drbagyizoltan@gmail.com>
2026-04-29 19:48:40 +0200

    feat: init fraud-detection-service with Spring Boot 3 and Java 21

---
+++ services/fraud-detection-service/src/test/java/hu/zoltanb/projects/fraud/FraudDetectionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.zoltanb.projects.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudDetectionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
