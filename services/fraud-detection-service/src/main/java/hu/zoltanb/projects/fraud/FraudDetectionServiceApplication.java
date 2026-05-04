fd4f96c497bf3c0fd0fbe02f94eda2318365a096
Zoltan Bagyi <drbagyizoltan@gmail.com>
2026-05-04 14:25:11 +0200

    fix: optimization of FraudAppConfig values

--- services/fraud-detection-service/src/main/java/hu/zoltanb/projects/fraud/FraudDetectionServiceApplication.java
+++ services/fraud-detection-service/src/main/java/hu/zoltanb/projects/fraud/FraudDetectionServiceApplication.java
@@ -1,18 +1,22 @@
 package hu.zoltanb.projects.fraud;
 
-import hu.zoltanb.projects.fraud.service.TransactionProducer;
 import hu.zoltanb.projects.fraud.transactiongenerator.TransactionGeneratorService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafka;
 
-import java.sql.SQLOutput;
-
 @SpringBootApplication
 @EnableKafka
 public class FraudDetectionServiceApplication {
+
+    @Value("${fraud.generator.transaction-count}")
+    private int transactionCount;
+
+    @Value("${fraud.generator.initial-delay-ms}")
+    private long initialDelay;
 
     public static void main(String[] args) {
         SpringApplication.run(FraudDetectionServiceApplication.class, args);
@@ -23,10 +27,10 @@ public class FraudDetectionServiceApplication {
         return args -> {
             //waiting 3 sec to Kafka Consumer be ready
             System.out.println("waiting for Kafka start");
-            Thread.sleep(3000);
+            Thread.sleep(initialDelay);
             System.out.println("Starting mass data generation...");
             //Number of trades
-            generator.generateData(1_000);
+            generator.generateData(transactionCount);
 
             System.out.println("All transactions are sent to Kafka!");
 
